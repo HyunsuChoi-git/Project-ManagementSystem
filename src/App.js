ce47e820e2bc3a910598062438169e6ca8d085d1
HyunsuChoi-git <choihs1054@gmail.com>
2022-06-20 14:46:54 +0900

    modify react version and install matorial ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -30,30 +48,40 @@ const customers = [
 ]
 
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return(
-            <Customer
-              key={c.id}
-              id={c.id}
-              name={c.name}
-              image={c.image}
-              gender={c.gender}
-              birthday={c.birthday}
-              job={c.job}
-            />
-          )
-        })
-      }
-    </div>
-        // 출력하고자 하는 데이터를 컴포넌트에 보내준다.
+class App extends Component{
+    render() {
+      const { classes } = this.props;
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              { customers.map(c => 
+                  { return( <Customer key={c.id} id={c.id} name={c.name} image={c.image} gender={c.gender} birthday={c.birthday} job={c.job} /> ) }
+                ) }
+            </TableBody>
+          </Table>
+        </Paper>
+            // 출력하고자 하는 데이터를 컴포넌트에 보내준다.
 
-        // map 함수를 이용하여 배열 형태의 반복되는 데이터를 추출할 수 있다.
-        // map 함수를 이용할 때에는 'key' 설정으로 각 데이터 고유의 값을 지정해주어야 한다,
-  );
+            // map 함수를 이용하여 배열 형태의 반복되는 데이터를 추출할 수 있다.
+            // map 함수를 이용할 때에는 'key' 설정으로 각 데이터 고유의 값을 지정해주어야 한다.
+
+            // Material iu 를 통해 Table 형태로 출력해줄 수 있다.
+            // Table 태그로 감싸고, 데이터가 출력되는 부분은 통상 TableBody 태그 내부에 담아주는 것이 룰이다.
+            // div --> Paper 태그로 변경. Paper는 컴포넌트의 외부를 감싸기위해 사용하는 컴포넌트 중 하나.
+      );
+    }
 }
 
-export default App;
+//export default withStyles(styles)(App);
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,41 +1,62 @@
 import React from 'react'
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
-    render(){
-        // 항상 수행되는 내용이 들어감.
-        return (
-            <div>
-                <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-                
-            </div>
-        // 컴포넌트를 class화 하여 두개의 컴포넌트로 만들어보기 (계층적 구성 react의 강력한 view 기능!!)
-        // (jsx문법) 리액트 요소를 반환할 때 두개 이상의 내부 데이터를 반환할 경우 div와 같은 태그로 감싸주어야 함 
-        )
-    }
-}
-
-class CustomerProfile extends React.Component {
     render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render(){
         return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"></img></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+
         )
     }
 }
 
 export default Customer;
+
+
+// class Customer extends React.Component {
+//     render(){
+//         // 항상 수행되는 내용이 들어감.
+//         return (
+//             <div>
+//                 <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image} />
+//                 <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
+                
+//             </div>
+//         // 컴포넌트를 class화 하여 두개의 컴포넌트로 만들어보기 (계층적 구성 react의 강력한 view 기능!!)
+//         // (jsx문법) 리액트 요소를 반환할 때 두개 이상의 내부 데이터를 반환할 경우 div와 같은 태그로 감싸주어야 함 
+//         )
+//     }
+// }
+
+// class CustomerProfile extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <img src={this.props.image} alt="profile"/>
+//                 <h2>{this.props.name}({this.props.id})</h2>
+//             </div>
+//         )
+//     }
+// }
+
+// class CustomerInfo extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//                 <p>{this.props.birthday}</p>
+//                 <p>{this.props.gender}</p>
+//                 <p>{this.props.job}</p>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Customer;
