bcf94d9dd9da824d8eb13ac7ce869db7d496c3ee
HyunsuChoi-git <choihs1054@gmail.com>
2022-06-22 17:05:38 +0900

    Create Customer DB Table And Node.js DB Connection Setting

--- server.js
+++ server.js
@@ -7,35 +7,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 // 위 코드는 node.js express를 사용할 때 일반적으로 작성하는 코드
 
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id' : 1,
-        'image' : 'http://placeimg.com/64/64/1',
-        'name' : '홍길동',
-        'birthday' : 961222,
-        'gender' : '남자',
-        'job' : '대학생'
-        },
-        {
-        'id' : 2,
-        'image' : 'http://placeimg.com/64/64/2',
-        'name' : '이순신',
-        'birthday' : 931222,
-        'gender' : '남자',
-        'job' : '포토그래퍼'
-        },
-        {
-        'id' : 3,
-        'image' : 'http://placeimg.com/64/64/3',
-        'name' : '대장금',
-        'birthday' : 941222,
-        'gender' : '여자',
-        'job' : '디자이너'
-        }
-  
-    ])
+//---- DT CONNECTION -----//
+const fs = require('fs');
+
+const dtInfo = fs.readFileSync('./database.json');
+const conf = JSON.parse(dtInfo);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
 });
+
+connection.connect();
+//////////////////////////////
+
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+});
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port} !`));
 
